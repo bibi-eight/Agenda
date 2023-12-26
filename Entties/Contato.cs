34c4f479e301b086ad1d73a604d86e33ce4ee257
Brenda Brito <bbritob8@gmail.com>
2023-12-26 10:16:15 -0300

    ?

--- Entties/Contato.cs
+++ Entties/Contato.cs
@@ -2,6 +2,7 @@ namespace Agenda.Entties
 {
     public class Contato
     {
+        public int Id {get; set;}
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
